7c4ad3ca33e1813e2385e181f4a05d77c795ba7b
pamepeixinho <pameiupip@gmail.com>
2018-08-19 15:50:16 -0300

    clean alllll funcking data

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 
 class Data:
@@ -36,8 +37,64 @@ class Data:
         with open('videos-v2.json', encoding='utf-8') as data_json:
             self.data_set = json.load(data_json)
 
-    def clean_dataset(self):
+    def treat_dataset(self):
+        self.get_average_of_emotions()
+        self.get_commitment_by_questions()
+        self.clean_data()
+        self.export_as_csv()
         return self.data_set
+
+    def export_as_csv(self):
+        data = self.data_set['dataset']
+        keys = data[0].keys()
+
+        with open('dataset.csv', 'w') as f:
+            dict_writer = csv.DictWriter(f, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(data)
+
+    def clean_data(self):
+        for person in self.data_set['dataset']:
+            del person['classLink']
+            del person['name']
+            del person['grade']
+            del person['__v']
+
+    def get_commitment_by_questions(self):
+        commitment_weight = 3
+        subject_weight = 2
+        didactics_weight = 1
+        rythm_weight = 1
+        qt_questions_used = 7
+
+        total_sum = commitment_weight + subject_weight + didactics_weight + rythm_weight
+
+        for person in self.data_set['dataset']:
+            sum_commitment = person['continueVideosLikeThis'] + person['goodExperience'] + person['haveCommitment']
+            sum_subject = person['interesting']
+            sum_didactics = person['likeDidactics'] + person['goodExamples']
+            sum_rythm = person['noPause']
+
+            commitment_score = ((commitment_weight * sum_commitment) +
+                                (subject_weight * sum_subject) +
+                                (didactics_weight * sum_didactics) +
+                                (rythm_weight * sum_rythm))
+
+            person['commitment_score'] = (commitment_score / total_sum) * (1.0/qt_questions_used)
+
+            del person['attention']
+            del person['continueVideosLikeThis']
+            del person['goodExamples']
+            del person['goodExperience']
+            del person['haveCommitment']
+            del person['interesting']
+            del person['likeDidactics']
+            del person['longVideo']
+            del person['moreDynamic']
+            del person['noPause']
+            del person['previousKnowledge']
+            del person['recommendVideo']
+            del person['tooMuchContent']
 
     def get_average_of_emotions(self):
         """
@@ -102,18 +159,18 @@ class Data:
             average_sadness /= emotions_size
             average_surprise /= emotions_size
 
-            person['averageAnger'] = average_anger
-            person['averageContempt'] = average_contempt
-            person['averageFear'] = average_fear
-            person['averageDisgust'] = average_disgust
-            person['averageHappiness'] = average_happiness
-            person['averageNeutral'] = average_neutral
-            person['averageSadness'] = average_sadness
-            person['averageSurprise'] = average_surprise
+            person['average_anger'] = average_anger
+            person['average_contempt'] = average_contempt
+            person['average_fear'] = average_fear
+            person['average_disgust'] = average_disgust
+            person['average_happiness'] = average_happiness
+            person['average_neutral'] = average_neutral
+            person['average_sadness'] = average_sadness
+            person['average_surprise'] = average_surprise
 
             del person['emotions']
-        print(self.data_set)
+        # print(self.data_set)
+
 
 testing = Data()
-# print(testing.data_set)
-testing.get_average_of_emotions()
+testing.treat_dataset()
