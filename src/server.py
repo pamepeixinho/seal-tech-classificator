a2ddc222618ee0eb98dd249fdcb3e10a204a7228
pamepeixinho <pameiupip@gmail.com>
2018-08-24 08:57:43 -0300

    fix endpoint to test

--- src/server.py
+++ src/server.py
@@ -58,6 +58,19 @@ def convert(value):
 
 @app.route('/predict')
 def predict():
+    emotionsDict = request.args
+    emotions = [[
+        convert(emotionsDict["anger"]), convert(emotionsDict["contempt"]),
+        convert(emotionsDict["fear"]), convert(emotionsDict["disgust"]),
+        convert(emotionsDict["happiness"]), convert(emotionsDict["neutral"]),
+        convert(emotionsDict["sadness"]), convert(emotionsDict["surprise"])
+    ]]
+
+    predictedCommitment = globalPredictor.predict(emotions)
+    return jsonify(commitment=predictedCommitment[0])
+
+@app.route('/predict-average')
+def predictAverage():
     # ?average_anger=0.976&average_contempt=0.00074&average_fear=0.000384723&average_disgust=0.0000000006&average_happiness=0.8&average_neutral=0.9&average_sadness=0&average_surprise=0
 
     # \?average_anger\=0.976\&average_contempt\=0.00074\&average_fear\=0.000384723\&average_disgust\=0.0000000006\&average_happiness\=0.8\&average_neutral\=0.9\&average_sadness\=0\&average_surprise\=0
