a8f7cb93de855a5bd9813e3f264940412e113347
pamepeixinho <pameiupip@gmail.com>
2018-08-24 07:33:03 -0300

    testing

---
+++ src/TestingScikitLearn.py
@@ -0,0 +1,42 @@
+from sklearn import tree
+import pandas
+import numpy
+from sklearn.externals import joblib
+
+class Predictor:
+    dataset_filename = 'dataset.csv'
+    model_filename = 'model.pkl'
+
+    def __init__(self):
+        self.inputs = []
+        self.targets = []
+
+    def _load_csv(self):
+        with open(self.dataset_filename) as csvfile:
+            data = pandas.read_csv(csvfile)
+            self.data_set = numpy.array(data)
+
+    def _split_input_target_from_csv(self):
+        self.inputs = self.data_set[:, 0:8]
+        self.targets = self.data_set[:, 8]
+        print(self.inputs)
+        print(self.targets)
+
+    def _save_model(self):
+        joblib.dump(self.clf, self.model_filename)
+
+    def load_model(self):
+        self.clf = joblib.load(self.model_filename)
+
+    def train(self):
+        self._load_csv()
+        self._split_input_target_from_csv()
+
+        self.clf = tree.DecisionTreeRegressor()
+
+        self.clf.fit(self.inputs, self.targets)
+        self._save_model()
+
+
+predictor = Predictor()
+predictor.train()
