107032eb0361392a80699776f2a61f715e05f847
Pamela Peixinho <pamela.peixinho@quintoandar.com.br>
2018-08-24 07:54:21 -0300

    Testing (#9)
    * testing
    
    * endpoints
    
    * fix endpoint to predict

--- src/server.py
+++ src/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from commitmentClassificator import NeuralNetworks
+from TestingScikitLearn import Predictor
 
 appName = 'FocaAi'
 
@@ -8,6 +9,8 @@ my_dev_port = 5000
 
 app = Flask(appName)
 
+globalPredictor = Predictor()
+globalPredictor.load_model()
 
 def setup_app(app):
     return 1
@@ -40,6 +43,35 @@ def classificate():
     emotions = request.args
     commitment = NeuralNetworks.classificate(emotions=emotions)
     return jsonify(commitment=commitment)
+
+
+@app.route('/prediction-train')
+def predictionTrain():
+    predictor = Predictor()
+    predictor.train()
+    globalPredictor.load_model()
+
+
+def convert(value):
+    return float(value)
+
+
+@app.route('/predict')
+def predict():
+    # ?average_anger=0.976&average_contempt=0.00074&average_fear=0.000384723&average_disgust=0.0000000006&average_happiness=0.8&average_neutral=0.9&average_sadness=0&average_surprise=0
+
+    # \?average_anger\=0.976\&average_contempt\=0.00074\&average_fear\=0.000384723\&average_disgust\=0.0000000006\&average_happiness\=0.8\&average_neutral\=0.9\&average_sadness\=0\&average_surprise\=0
+
+    emotionsDict = request.args
+    emotions = [[
+        convert(emotionsDict["average_anger"]), convert(emotionsDict["average_contempt"]),
+        convert(emotionsDict["average_fear"]), convert(emotionsDict["average_disgust"]),
+        convert(emotionsDict["average_happiness"]), convert(emotionsDict["average_neutral"]),
+        convert(emotionsDict["average_sadness"]), convert(emotionsDict["average_surprise"])
+    ]]
+
+    predictedCommitment = globalPredictor.predict(emotions)
+    return jsonify(commitment=predictedCommitment[0])
 
 setup_app(app)
 
